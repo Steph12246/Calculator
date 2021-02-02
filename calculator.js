c52aed799cc0a425199e03a329e8c0226a63395f
Steph <steph248@outlook.com>
2021-02-02 15:24:43 +0000

    display numbers and AC

--- calculator.js
+++ calculator.js
@@ -1,8 +1,59 @@
-const numKeys = document.querySelectorAll('num');
-  numKeys.forEach(numKey => numKey.addEventListener('click', function(){displayNum()}));
+var allKeys = document.querySelectorAll('.keys');
 const display = document.getElementById('display');
+// do I need this line below?
 var inputNo = '';
 
+allKeys.forEach(allKeys => allKeys.addEventListener('click', function(e){
+  const action = e.target.dataset.action
+  if (!action && display.innerHTML === 0 || !action && display.innerHTML === ''){
+    display.innerHTML= allKeys.innerHTML;
+  }
+    else if (!action) {
+ const keyContent = e.target.textContent
+ const displayedNum = display.textContent
+ display.textContent = displayedNum + keyContent
+    }
+    else switch(action){
+      case 'back':
+      console.log('go back');
+      //remove str.sybstring(0, str.length -1);
+        break;
+      case 'clear':
+      display.textContent = "";
+      let a="";
+      let b="";
+        break;
+      case 'divide':
+        break;
+      case 'multiply':
+        break;
+      case 'minus':
+        break;
+      case 'plus':
+        break;
+      case 'decimal':
+        break;
+      case 'calculate':
+      // operate (a, operator, b)
+        break;
+      default: // Return with same tex content
+        break;
+    }
+}));
+// if pressed key = operator then displayedNum = a and pressed key = operator
+// and display.textContent= displayedNum + " " + keyContent + " "
+// if pressed key is =(/ another operator) then displayedNum = b return a, b and operator and run operate()
+// need to get the display.textContent to equal a
+// get the action to equal the operator
+// the display.textcontent after operator to equal b
+//let a = 0;
+//  let b = 1;
+//  for (let i = 1; i < count; i++) {
+//    const temp = b;
+//    b = a + b;
+//    a = temp;
+//  }
+//  return b;
 
 function add (a, b){
   return a + b;
@@ -35,10 +86,4 @@ function operate (a, operator, b){
       return ab;
       break;
   }
-}
-
-function displayNum(){
-  console,log('heya');
-  inputNo = numKeys.innerHTML
-  display.innerHTML = inputNo
 }
