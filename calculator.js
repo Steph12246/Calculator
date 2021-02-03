8fa4fba4239ebe5315d26bd25db6fa7bcc3e5775
Steph <steph248@outlook.com>
2021-02-03 15:25:29 +0000

    basic calculations

--- calculator.js
+++ calculator.js
@@ -6,123 +6,144 @@ let b="";
 allKeys.forEach(allKeys => allKeys.addEventListener('click', function(e){
   const action = e.target.dataset.action
   const keyContent = e.target.textContent
-  if (!action && display.innerHTML === 0 || !action && display.innerHTML === ''){
-    display.innerHTML= allKeys.innerHTML;
-  }
-    else if (!action) {
- const displayedNum = display.textContent
- display.textContent = displayedNum + keyContent
-    }
-    else switch(action){
-      case 'back':
-      console.log('go back');
-      var str = display.textContent
-      var newStr = str.slice(0, -1)
-      display.textContent = newStr;
-      console.log(display.textContent);
-      console.log(a);
-        break;
-      case 'clear':
-      display.textContent = "";
-       a="";
-       b="";
-        break;
-      case 'divide':
-       if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
-       || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
-         console.log('already');
-         b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
-         console.log(b);
-         operate(a, operator, b);
-         break;
-      }
-      else if (display.textContent === ""|| display.textContent === "0"){
-          return;
-        }
-      else {
-           a = display.textContent;
-           operator = 'divide'
-          display.textContent = display.textContent + keyContent;
-          console.log(a);
-          console.log(operator);
-        break;
-      }
 
-      // when divide is pressed check if an opertor has already been pressed if yes
-      // store number after previous operator as b and evaluate previous
-      // calculation. Store new value as a. If no, store displayed num as a and operator as divide
-        break;
-      case 'multiply':
-          a = display.textContent;
-          operator = 'multiply'
-          display.textContent = display.textContent + keyContent;
-          console.log(a);
-          console.log(operator);
-        break;
-      case 'minus':
-          a = display.textContent;
-          operator = 'multiply'
-          display.textContent = display.textContent + keyContent;
-          console.log(a);
-          console.log(operator);
-        break;
-      case 'plus':
-          a = display.textContent;
-          operator = 'multiply'
-          display.textContent = display.textContent + keyContent;
-          console.log(a);
-          console.log(operator);
-        break;
-      case 'decimal':
-        break;
-      case 'calculate':
-      // operate (a, operator, b)
-      // if 'divide by 0' return: "surely you know this one? Hint it's infinity"
-      // display.textContent = new value
-        break;
-      default: // Return with same tex content
-        break;
-    }
-}));
-// if pressed key = operator then displayedNum = a and pressed key = operator
-// and display.textContent= displayedNum + " " + keyContent + " "
-// if pressed key is =(/ another operator) then displayedNum = b return a, b and operator and run operate()
-// need to get the display.textContent to equal a
-// get the action to equal the operator
-// the display.textcontent after operator to equal b
-//let a = 0;
-//  let b = 1;
-//  for (let i = 1; i < count; i++) {
-//    const temp = b;
-//    b = a + b;
-//    a = temp;
-//  }
-//  return b;
+if (!action && display.textContent === '0' || !action && display.innerHTML === ''){
+          display.textContent= allKeys.textContent;
+          }
+else if (!action) {
+          const displayedNum = display.textContent
+          display.textContent = displayedNum + keyContent
+          }
+else switch(action){
+case 'divide':
+           if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
+           || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
+             console.log('already');
+             b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+             operate(a, operator, b);
+             break;
+          }
+          else if (display.textContent === ""|| display.textContent === "0"){
+              return;
+            }
+          else {
+               a = display.textContent;
+               operator = 'divide'
+              display.textContent = display.textContent + keyContent;
+          break;
+          }
+case 'multiply':
+            if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
+            || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
+            console.log('already');
+            b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+            operate(a, operator, b);
+              break;
+              }
+            else if (display.textContent === ""){
+              return;
+              break;
+              }
+            else {
+              a = display.textContent;
+              operator = 'multiply'
+              display.textContent = display.textContent + keyContent;
+                break;
+                }
+case 'minus':
+          if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
+          || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
+          console.log('already');
+          b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+          operate(a, operator, b);
+            break;
+            }
+          else if (display.textContent === ""){
+            display.textContent= keyContent;
+            break;
+            }
+          else {
+            a = display.textContent;
+            operator = 'minus'
+            display.textContent = display.textContent + keyContent;
+            break;
+            }
+case 'plus':
+            if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
+            || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
+            console.log('already');
+            b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+            operate(a, operator, b);
+              break;
+              }
+            else if (display.textContent === ""){
+              return;
+              break;
+              }
+            else {
+              a = display.textContent;
+              operator = 'plus'
+              display.textContent = display.textContent + keyContent;
+                break;
+                }
+case 'decimal':
+            display.textContent = display.textContent + keyContent;
+                  break;
+case 'calculate':
+            b = display.textContent.split(/[\/\*\+\-]/).pop()
+            operate(a, operator, b);
+                  break;
+case 'back':
+            console.log('go back');
+            var str = display.textContent
+            var newStr = str.slice(0, -1)
+            display.textContent = newStr;
+                  break;
+case 'clear':
+            display.textContent = "";
+            a="";
+            b="";
+                  break;
+default:
+                  break;
+}}));
 
+
+// make it so if you press another operator it works
+// when using a decimal get rid of the extra million 0s
+// display.textContent = new value
+// show value to 6 decimal places
+// make sure the a value updates. I think it does already..
 function add (a, b){
+  // convert from string to number
   // remove any newOperators
-  b = // b - operator
-  a = a + b;
-  display.textContent = a + operator
+  //b = // b - operator
+  //a = a + b;
+  //display.textContent = a + operator
   // return newOperator
+  display.textContent = a + b; //this is currently a string...
 }
 function subtract (a, b){
-  return a - b;
+  display.textContent = a - b;
 }
 function multiply (a,b){
-  return a * b;
+  display.textContent = a * b;
 }
 function divide (a, b){
+  // if b === 0 return: "surely you know this one? Hint it's infinity"
   // if b contains an operator sice operator then update b
-  return a/b; // plus removed operator
+  display.textContent = a/b; // plus removed operator
 }
 
 function operate (a, operator, b){
+  console.log(a);
+  console.log(operator);
+  console.log(b);
   switch (operator){
-    case 'add':
+    case 'plus':
       add(a,b);
       break;
-    case 'subtract':
+    case 'minus':
       subtract(a,b);
       break;
     case 'multiply':
@@ -132,7 +153,7 @@ function operate (a, operator, b){
       divide(a,b);
       break;
     default:
-      return ab;
+      return a;
       break;
   }
 }
