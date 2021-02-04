7c3634dd8cd0dfa1627ce4c25716dec82621431e
Steph <steph248@outlook.com>
2021-02-04 15:21:42 +0000

    string of numbers

--- calculator.js
+++ calculator.js
@@ -2,12 +2,15 @@ var allKeys = document.querySelectorAll('.keys');
 const display = document.getElementById('display');
 let a="";
 let b="";
+let operator="";
+
 
 allKeys.forEach(allKeys => allKeys.addEventListener('click', function(e){
   const action = e.target.dataset.action
   const keyContent = e.target.textContent
 
-if (!action && display.textContent === '0' || !action && display.innerHTML === ''){
+if (!action && display.textContent === '0' || !action && display.textContent === ''
+|| !action && display.textContent === 'NaN'){
           display.textContent= allKeys.textContent;
           }
 else if (!action) {
@@ -16,14 +19,25 @@ else if (!action) {
           }
 else switch(action){
 case 'divide':
+          if (display.textContent.slice(-1) === "."){
+            a = display.textContent;
+            operator= 'divide';
+            display.textContent = display.textContent + 0 + keyContent;
+            return;
+          }
            if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
            || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
              console.log('already');
-             b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+             b = display.textContent.split(/[\/\*\+\-]/).pop();
              operate(a, operator, b);
+             operator = 'divide';
+             a = display.textContent;
+             console.log(a);
+             display.textContent = a + keyContent;
              break;
           }
-          else if (display.textContent === ""|| display.textContent === "0"){
+          else if (display.textContent === ""|| display.textContent === "0"
+          || display.textContent === 'NaN'){
               return;
             }
           else {
@@ -32,15 +46,24 @@ case 'divide':
               display.textContent = display.textContent + keyContent;
           break;
           }
+
 case 'multiply':
+            if (display.textContent.slice(-1) === "."){
+              a = display.textContent;
+              operator= 'multiply';
+              display.textContent = display.textContent + 0 + keyContent;
+              return;
+            }
             if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
             || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
-            console.log('already');
-            b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+            b = display.textContent.split(/[\/\*\+\-]/).pop();
             operate(a, operator, b);
+            operator = 'multiply';
+            a = display.textContent;
+            display.textContent = a + keyContent;
               break;
               }
-            else if (display.textContent === ""){
+            else if (display.textContent === "" || display.textContent === 'NaN'){
               return;
               break;
               }
@@ -51,17 +74,31 @@ case 'multiply':
                 break;
                 }
 case 'minus':
+// if there are 2 or more - in the sentence eval.
+// if 2 in a row change to add
+          if (display.textContent.slice(-1) === "."){
+            a = display.textContent;
+            operator= 'minus';
+            display.textContent = display.textContent + 0 + keyContent;
+            return;
+          }
           if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
           || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
           console.log('already');
-          b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+          b = display.textContent.split(/[\/\*\+\-]/).pop();
           operate(a, operator, b);
+          operator = 'divide';
+          a = display.textContent;
+          display.textContent = a + keyContent;
             break;
             }
           else if (display.textContent === ""){
             display.textContent= keyContent;
             break;
             }
+          else if (display.textContent === 'NaN'){
+            return;
+          }
           else {
             a = display.textContent;
             operator = 'minus'
@@ -69,14 +106,23 @@ case 'minus':
             break;
             }
 case 'plus':
+            if (display.textContent.slice(-1) === "."){
+              a = display.textContent;
+              operator= 'plus';
+              display.textContent = display.textContent + 0 + keyContent;
+              return;
+            }
             if ((display.textContent.indexOf('+')> -1)||(display.textContent.indexOf('/^[0-9]/-')> -1)
             || (display.textContent.indexOf('/')> -1) || (display.textContent.indexOf('*')> -1)) {
             console.log('already');
-            b = display.textContent.split(/[\/\*\+\-]/).pop() + keyContent;
+            b = display.textContent.split(/[\/\*\+\-]/).pop();
             operate(a, operator, b);
+            operator = 'plus';
+            a = display.textContent;
+            display.textContent = a + keyContent;
               break;
               }
-            else if (display.textContent === ""){
+            else if (display.textContent === "" || display.textContent === 'NaN'){
               return;
               break;
               }
@@ -87,41 +133,81 @@ case 'plus':
                 break;
                 }
 case 'decimal':
+            let currentNum = display.textContent.split(/[\/\*\+\-]/).pop();
+            let previousChar = display.textContent.substring(display.textContent.length-1);
+            if (currentNum.indexOf('.')> -1){
+              return;
+            }
+            else if (a === "" && display.textContent === "" ){
+              display.textContent = 0 + keyContent;
+              return;
+            }
+            else if (previousChar==="*" || previousChar==="/" || previousChar==="+"
+            || previousChar==="-" ){
+              display.textContent = display.textContent + 0 +keyContent;
+              return;
+            }
             display.textContent = display.textContent + keyContent;
                   break;
 case 'calculate':
-            b = display.textContent.split(/[\/\*\+\-]/).pop()
+            if (display.textContent === ""){
+              return;
+            }
+            if (b === ""){
+              b = display.textContent.split(/[\/\*\+\-]/).pop()
+            }
+            else {
+              b = b;
+            }
+            if (operator === ""){
+              return;
+            }
+            else {
             operate(a, operator, b);
+            a = display.textContent;
+            
+            console.log(a);
+            console.log(operator);
+            console.log(b);
                   break;
+                };
 case 'back':
             console.log('go back');
             var str = display.textContent
             var newStr = str.slice(0, -1)
             display.textContent = newStr;
+            if (newStr === ""){
+              a="";
+              operator="";
+              b="";
+            }
                   break;
 case 'clear':
             display.textContent = "";
             a="";
             b="";
+            operator="";
                   break;
 default:
                   break;
 }}));
 
-
+// if display is NaN clear screen before being able to press another button
 // make it so if you press another operator it works
 // when using a decimal get rid of the extra million 0s
 // display.textContent = new value
 // show value to 6 decimal places
 // make sure the a value updates. I think it does already..
+// convert from string to number
+// remove any newOperators
+//b = // b - operator
+//a = a + b;
+//display.textContent = a + operator
+// return newOperator
+
+
 function add (a, b){
-  // convert from string to number
-  // remove any newOperators
-  //b = // b - operator
-  //a = a + b;
-  //display.textContent = a + operator
-  // return newOperator
-  display.textContent = a + b; //this is currently a string...
+  display.textContent = +a + +b;
 }
 function subtract (a, b){
   display.textContent = a - b;
@@ -130,10 +216,15 @@ function multiply (a,b){
   display.textContent = a * b;
 }
 function divide (a, b){
-  // if b === 0 return: "surely you know this one? Hint it's infinity"
-  // if b contains an operator sice operator then update b
-  display.textContent = a/b; // plus removed operator
+if (b === '0'){
+  console.log('hmm');
+  divZero = "It's infinite!"
+  display.textContent = divZero;
 }
+else {
+  display.textContent= a/b;
+}}
+
 
 function operate (a, operator, b){
   console.log(a);
@@ -147,6 +238,7 @@ function operate (a, operator, b){
       subtract(a,b);
       break;
     case 'multiply':
+    console.log('here');
       multiply(a,b);
       break;
     case 'divide':
@@ -157,8 +249,8 @@ function operate (a, operator, b){
       break;
   }
 }
-
-
+// after pressing equals the answer should pop up and then clear the screen once another number is pressed
+// get keyboard shortcut to work
 // Get font digital-7 to apear when the numbers are typed in. While display number is
 // equal to 07734, 14, 5508.14, 5318008 rotate the display div and maybe round and
 // unround the css edges
